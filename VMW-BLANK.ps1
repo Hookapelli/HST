6ba14eb43ff910e5e1b9b10470bbd1ed2dd3ed47
Hookapelli <Hookapelli@gmail.com>
2025-03-21 18:06:57 +0200

    done

--- VMW-BLANK.ps1
+++ VMW-BLANK.ps1
@@ -1,4 +1,22 @@
-# !!!!!!!!!!!! DON'T FORGET TO CHANGE THE VMW CONTROLL ID !!!!!!!!!!
+Function fn_VMW-NSX_TEST {   # Determine if Defauly Layer 3 Rule is configured to DROP
+  Write-Host "VMW-NSX_01429" -ForegroundColor Green
+  $uri = "https://$global:NSXmgr/policy/api/v1/infra/tier-0s"
+  $command = "curl -k -s -X GET -H 'Cookie: JSESSIONID=$global:jsessionid' -H 'X-XSRF-TOKEN: $global:xxsrftoken ' $uri --insecure"
+  $response = Invoke-Expression $command | ConvertFrom-Json
+    if ($response.result_count -eq "0") { Write-Host "No T0 Configured"} else { 
+      foreach ($result in $response.results) {
+        $t0id=$result.id 
+        $uri = "https://$global:NSXmgr/policy/api/v1/infra/tier-0s/$t0id/gateway-firewall"
+        $command = "curl -k -s -X GET -H 'Cookie: JSESSIONID=$global:jsessionid' -H 'X-XSRF-TOKEN: $global:xxsrftoken ' $uri --insecure"
+        $t0data = Invoke-Expression $command | ConvertFrom-Json
+          foreach ($policy in $t0data.results) {
+            foreach ($rules in $policy.rules) {
+              Write-Host $rules.display_name - $rules.logged
+            } 
+        }  
+      }           
+    }
+}
 
 # CUT BELOW HERE ------------------------
 Function fn_SetVars {
@@ -50,4 +68,4 @@ Function fn_RequestNSXToken {
 }
 fn_SetVars
 fn_RequestNSXToken
-fn_VMW-NSX_01409 
+fn_VMW-NSX_TEST
