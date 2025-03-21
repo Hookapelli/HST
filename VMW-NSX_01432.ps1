2486b20664296c4f0c1a3061ab8eaba26479ec5e
Hookapelli <Hookapelli@gmail.com>
2025-03-21 22:28:19 +0200

    01432 Completed

--- VMW-NSX_01432.ps1
+++ VMW-NSX_01432.ps1
@@ -1,14 +1,16 @@
 Function fn_VMW-NSX_01432 {
   Write-Host "VMW-NSX_01432" -ForegroundColor Green
-  # Perform API Call 
   $uri = "https://$global:NSXmgr/policy/api/v1/infra/tier-0s"
   $command = "curl -k -s -X GET -H 'Cookie: JSESSIONID=$global:jsessionid' -H 'X-XSRF-TOKEN: $global:xxsrftoken ' $uri --insecure"
   $response = Invoke-Expression $command | ConvertFrom-Json
-  if ($response.result_count -eq "0") { Write-Host "No T0 Configured"} else {
+  if ($response.result_count -eq "0") { Write-Host "No T1 Deployed"} else {
+    foreach ($result in $response.results){
+      $t0id = $result.id
+      $uri =  "https://$global:NSXmgr/policy/api/v1/infra/domains/default/gateway-policies/Policy_Default_Infra-tier0-$t0id/rules/default_rule"
+      $command2 = "curl -k -s -X GET -H 'Cookie: JSESSIONID=$global:jsessionid' -H 'X-XSRF-TOKEN: $global:xxsrftoken ' $uri --insecure"
+      $defaultRule = Invoke-Expression $command2 | ConvertFrom-Json
+      Write-Host $result.display_name" Default Rule is: "$defaultRule.action
 
-    # For each T0R: https://$global:NSXmgr/policy/api/v1/infra/domains/default/gateway-policies/Policy_Default_Infra-tier0-#{t0id}/rules/default_rule
-      # action -ne 'ALLOW'
+    }
   }
-  Write-Host "-------------------------------------------------------"
-  Write-Host
 }
