fba3e867d9def8782e6fbe0405ba311154b3ba7e
HookaPelli <hookapelli@gmail.com>
2025-03-21 11:52:48 -0400

    Update VMW-NSX_01429.ps1

--- VMW-NSX_01429.ps1
+++ VMW-NSX_01429.ps1
@@ -1,50 +1,4 @@
-Function fn_SetVars {
-    $global:NSXmgr = '192.168.50.4'
-    $global:NSXTAdminUser = 'admin'
-    $global:NSXTAdminPass = 'VMware1!VMware1!'
-  }
-  
-  Function fn_RequestNSXToken {
-    Connect-NsxServer $global:NSXmgr -User $global:NSXTAdminUser -Password $global:NSXTAdminPass
-    Write-Host "Preparing NSX-T Manager API Token..." -ForegroundColor Green
-  
-    # Execute curl Command and Capture Output with Headers
-    $url = "https://$global:NSXmgr/api/session/create"
-    $command = "curl -k -s -D -c -X POST -d 'j_username=$global:NSXTAdminUser&j_password=$global:NSXTAdminPass' -i 2>&1 $url --insecure"
-    $responseString = Invoke-Expression $command
-  
-    # Extract JSESSIONID from Header
-    $regex = [regex]::Match($responseString, "JSESSIONID=([\w\d]{32})")
-  
-    # Check if a Match Was Found
-    if ($regex.Success) {
-        $global:jsessionid = $regex.Groups[1].Value
-    } else {
-        Write-Output "JSESSIONID not found or not enough characters available."
-    }
-  
-    # Extract X-XSRF-TOKEN from Headers
-    $regex = [regex]::Match($responseString, "x-xsrf-token:\s*([\w\d-]+)")
-  
-    # Check if a Match Was Found
-    if ($regex.Success) {
-      $global:xxsrftoken = $regex.Groups[1].Value
-    } else {
-        Write-Output "X-XSRF-TOKEN not found or not enough characters available."
-    }
-  
-    Write-Host "JSESSION:..."$global:jsessionid -ForegroundColor DarkYellow
-    Write-Host "X-XSRF-TOKEN:..."$global:xxsrftoken -ForegroundColor DarkYellow
-    <#$uri = "https://$global:NSXmgr/api/v1/aaa/registration-token"
-    $command = "curl -k -s -X POST -H 'Cookie: JSESSIONID=$global:jsessionid' -H 'X-XSRF-TOKEN: $global:xxsrftoken' 'j_username=$global:NSXTAdminUser&j_password=$global:NSXTAdminPass ' $uri --insecure"
-    $Response = Invoke-Expression $command
-    $Response = $Response | ConvertFrom-Json
-    $global:btoken = $Response.token
-    Write-Host "Bearer Token: $global:btoken"  -ForegroundColor DarkYellow #>
-    Write-Host "-------------------------------------------------------"
-    Write-Host
-  }
-  Function fn_VMW-NSX_01429 {
+Function fn_VMW-NSX_01429 {
     Write-Host "VMW-NSX_01429" -ForegroundColor Green
     $uri = "https://$global:NSXmgr/policy/api/v1/infra/tier-0s"
     $command = "curl -k -s -X GET -H 'Cookie: JSESSIONID=$global:jsessionid' -H 'X-XSRF-TOKEN: $global:xxsrftoken ' $uri --insecure"
@@ -63,6 +17,3 @@ Function fn_SetVars {
         }           
       }
 }
-fn_SetVars
-fn_RequestNSXToken
-fn_VMW-NSX_01429 
