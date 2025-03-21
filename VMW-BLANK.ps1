2b7c79c4ae52104f64d8ded1fe3c1b0fbc8db694
Hookapelli <Hookapelli@gmail.com>
2025-03-21 22:23:35 +0200

    BLANK

--- VMW-BLANK.ps1
+++ VMW-BLANK.ps1
@@ -1,4 +1,20 @@
+Function fn_VMW-NSX_01431 {
+  Write-Host "VMW-NSX_01431"  -ForegroundColor Green
+  $uri = "https://$global:NSXmgr/policy/api/v1/infra/tier-0s"
+  $command = "curl -k -s -X GET -H 'Cookie: JSESSIONID=$global:jsessionid' -H 'X-XSRF-TOKEN: $global:xxsrftoken ' $uri --insecure"
+  $response = Invoke-Expression $command | ConvertFrom-Json
+  if ($response.result_count -eq "0") { Write-Host "No T1 Deployed"} else {
+    foreach ($result in $response.results){
+      $t0id = $result.id
+      $uri =  "https://$global:NSXmgr/policy/api/v1/infra/domains/default/gateway-policies/Policy_Default_Infra-tier0-$t0id/rules/default_rule"
+      $command2 = "curl -k -s -X GET -H 'Cookie: JSESSIONID=$global:jsessionid' -H 'X-XSRF-TOKEN: $global:xxsrftoken ' $uri --insecure"
+      $defaultRule = Invoke-Expression $command2 | ConvertFrom-Json
+      Write-Host $result.display_name" Default Rule is: "$defaultRule.action
 
+    }
+  }
+}
+ 
 # CUT BELOW HERE ------------------------
 Function fn_SetVars {
     $global:NSXmgr = '192.168.50.4'
