e360afb4d7932669f64ce1f9ce5c27589586058a
HookaPelli <hookapelli@gmail.com>
2025-04-15 06:15:17 -0800

    Update VMW-NSX_01503.ps1

--- VMW-NSX_01503.ps1
+++ VMW-NSX_01503.ps1
@@ -6,35 +6,65 @@ Function fn_VMW-NSX_01503 {
   $headers.Add("X-XSRF-TOKEN", $global:xxsrftoken)
   $headers.Add("Cookie", "JSESSIONID=$global:jsessionid")
 
-  $t0s = Invoke-WebRequest "https://$global:NSXmgr/policy/api/v1/infra/tier-0s" -Method 'GET' -Headers $headers 
-  $t0sjson = $t0s | ConvertFrom-Json 
+# Get Tier-0s if they exist
 
-  # Check if status is OK (200)
-  if ($t0s.StatusCode -ne 200) {
+try { 
+
+  $t0s = Invoke-WebRequest "https://$global:NSXmgr/policy/api/v1/infra/tier-0s" -Method 'GET' -Headers $headers  
+
+   } catch {
+
+     Write-Host "No Tier-0 Found"
+
+    }
+    
+    if ($t0s.StatusCode -ne 200) {
+
       Write-Host "Failed to retrieve Tier-0 Gateways"
+    }
+
+    if ($t0s) {
+
+            $t0sjson = $t0s | ConvertFrom-Json
+
   }
 
   if ($t0sjson.result_count -eq 0) {
-      Write-Host "No T0 Gateways are deployed. This is Not Applicable."
+
+    Write-Host "No T0 Gateways are deployed. This is Not Applicable."
+
   }
 
   Write-Host "Tier-0s Found:"$t0sjson.result_count
 
-  foreach ($t0s in $t0sjson.results) {
-      Write-Host "Tier-0:"$t0s.display_name 
-      if ($t0.dhcp_config_path -ne "null") {
-          Write-Host "DCHP Enabled on"$t0s.display_name
+  foreach ($t0 in $t0sjson.results) {
 
-          foreach ($dhcpprofile in $t0s.dhcp_config_paths){
-              $dhcpinfo = Invoke-WebRequest "https://$global:NSXmgr/policy/api/v1$dhcpprofile" -Method 'GET' -Headers $headers 
-              $dhcpinfojson = $dhcpinfo | ConvertFrom-Json 
+    Write-Host "Tier-0:"$t0.display_name 
 
-              Write-Host "   DHCP Profile"$dhcpinfojson.id 
-              Write-Host "      DHCP Server Address:" $dhcpinfojson.server_address
-              Write-Host "      DHCP Addresses:"$dhcpinfojson.server_addresses
-          } 
+      if ($t0.dhcp_config_paths) {
+
+        Write-Host "DCHP Enabled on"$t0.display_name
+
+        foreach ($dhcpprofile in $t0.dhcp_config_paths){
+
+          $dhcpinfo = Invoke-WebRequest "https://$global:NSXmgr/policy/api/v1$dhcpprofile" -Method 'GET' -Headers $headers        
+
+          $dhcpinfojson = $dhcpinfo | ConvertFrom-Json 
+
+          Write-Host "   DHCP Profile"$dhcpinfojson.id 
+
+          Write-Host "      DHCP Server Address:" $dhcpinfojson.server_address
+
+          Write-Host "      DHCP Addresses:"$dhcpinfojson.server_addresses # Should Match Input File Data
+
+        } 
+
       } else {
+
         Write-Host "No DHCP Servers Configured for"$t0s.display_name
+
     }
+
   }
-}    
+
+} 
