04fb927e001bac02bd5e8794a51f0354d67e14c9
HookaPelli <hookapelli@gmail.com>
2025-04-15 05:41:19 -0800

    Update VMW-NSX_01532.ps1

--- VMW-NSX_01532.ps1
+++ VMW-NSX_01532.ps1
@@ -6,35 +6,65 @@ Function fn_VMW-NSX_01532 {
   $headers.Add("X-XSRF-TOKEN", $global:xxsrftoken)
   $headers.Add("Cookie", "JSESSIONID=$global:jsessionid")
 
-  $t1s = Invoke-WebRequest "https://$global:NSXmgr/policy/api/v1/infra/tier-1s" -Method 'GET' -Headers $headers 
-  $t1sjson = $t1s | ConvertFrom-Json 
 
   # Check if status is OK (200)
-  if ($t1s.StatusCode -ne 200) {
+  try { 
+
+    $t1s = Invoke-WebRequest "https://$global:NSXmgr/policy/api/v1/infra/tier-1s" -Method 'GET' -Headers $headers  
+  
+    } catch {
+
+      Write-Host "No Tier-1 Found"
+
+    }
+      if ($t1s.StatusCode -ne 200) {
+
       Write-Host "Failed to retrieve Tier-1 Gateways"
   }
 
-  if ($t1sjson.result_count -eq 0) {
-      Write-Host "No T1 Gateways are deployed. This is Not Applicable."
+  if ($t1s) {
+
+    $t1sjson = $t1s | ConvertFrom-Json
+
   }
+
+  if ($t1sjson.result_count -eq 0) {
+
+    Write-Host "No T1 Gateways are deployed. This is Not Applicable."
+
+  }
+
 
   Write-Host "Tier-1s Found:"$t1sjson.result_count
 
   foreach ($t1s in $t1sjson.results) {
+
       Write-Host "Tier-1:"$t1s.display_name 
+
       if ($t1.dhcp_config_path) {
-          Write-Host "DCHP Enabled on"$t1s.display_name
+      
+        Write-Host "DCHP Enabled on"$t1s.display_name
 
           foreach ($dhcpprofile in $t1s.dhcp_config_paths){
-              $dhcpinfo = Invoke-WebRequest "https://$global:NSXmgr/policy/api/v1$dhcpprofile" -Method 'GET' -Headers $headers 
-              $dhcpinfojson = $dhcpinfo | ConvertFrom-Json 
+      
+            $dhcpinfo = Invoke-WebRequest "https://$global:NSXmgr/policy/api/v1$dhcpprofile" -Method 'GET' -Headers $headers 
+      
+            $dhcpinfojson = $dhcpinfo | ConvertFrom-Json 
 
               Write-Host "   DHCP Profile"$dhcpinfojson.id 
+      
               Write-Host "      DHCP Server Address:" $dhcpinfojson.server_address
-              Write-Host "      DHCP Addresses:"$dhcpinfojson.server_addresses
+      
+              Write-Host "      DHCP Addresses:"$dhcpinfojson.server_addresses # Shoild Match Input File Data
           } 
-      } else {
-        Write-Host "No DHCP Servers Configured for"$t1s.display_name
-    }
-  }
-}  
+      
+        } else {
+      
+          Write-Host "No DHCP Servers Configured for"$t1s.display_name
+    
+
+        }
+
+      }
+
+    }  
